07f47bb888f5b48b34710c669b663af79b6fe02d
Tamerlan <tamerlan.recebov062@gmail.com>
2024-02-08 04:38:29 +0400

    First h_w

---
+++ OneDrive/Desktop/c#seminar/Massiv_Seminar/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 2, 4, 6, 8, 10, 30, 70, 100, 10, 10 };
+int minRange = 10;
+int maxRange = 90;
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= minRange && array[i] <= maxRange)
+    {
+        count++;
+    }
+}
+System.Console.Write(count);
