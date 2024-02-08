f1f833d6143f8914be2643cb73761d2c79e05f90
Tamerlan <tamerlan.recebov062@gmail.com>
2024-02-08 05:11:47 +0400

    second h_w

---
+++ OneDrive/Desktop/c#seminar/Seminar_2/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = { 2, 5, 2, 3, 6, 3, 7, 8, 2, 4};
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}    
+System.Console.WriteLine(count);
